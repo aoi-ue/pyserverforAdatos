aa6c2dad8615290b3ed433a6b5f28ecdf862dc90
yuhanyuhan <yuhanlian@gmail.com>
2018-10-10 17:30:01 +0800

    Impleted server connection and request to do GET and POST

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import requests 
 import http.server 
 import socketserver
 
@@ -7,4 +8,24 @@ Handler = http.server.SimpleHTTPRequestHandler
 httpd = socketserver.TCPServer(("",PORT),Handler) 
 
 print ("Successfully created at PORT", PORT)
+
+#get http requesting from tasking server in config server tasking url /trigger task
+r = requests.get('https://swapi.co/api/people/1')
+
+if r.status_code == 200: 
+    print ("GET Request from Tasking Server is connected successfully")
+    print (r.status_code)
+else: 
+    print ("Try again!")
+    
+
+# function to run josie's code 
+
+# function for sentinel files to be compiled as images 
+
+# function to run hafiz's code 
+
+# function to store in aws database 
+
+
 httpd.serve_forever()
