9621778d81322b13d5371fde103519ab9d4e72b8
yuhanyuhan <yuhanlian@gmail.com>
2018-10-24 10:02:52 +0800

    Update progress on server

--- README.md
+++ README.md
@@ -1,23 +1,33 @@
 # pyserverforAdatos checklist
 
 ## Completed 
+
 - [x]  Establish the server requirements and understand how Sentinel Hub is imported to QGIS 
 - [x]  Create a simple pyserver that takes an HTTP request and send a request on insomnia 
 - [x]  Setting up python and conda environment locally 
 - [x]  Sentinel Hub running locally 
 - [x]  Implement to Python Server as a listening port to tasking_server and use GET method from taskingserver 
 - [x]  Implemented GET & POST using Bottle Library (https://bottlepy.org/docs/dev/index.html)  
+- [x]  Upon client's onclick that triggers tasking_server, write a script to
+    * GET Request from tasking_server endpoint
+    * Sync to aws S3 script buckets (aws cli & boto3)
+    * Retrieve/download using aws s3 cli script to download/read Josie's and Hafiz's most updated script
 
-## To be completed by 19 Oct 2018 (Friday) 
-- [ ]  Based on client's specific analysis key, retrieve, using asyncio, Josie's and Hafiz's most updated script from S3 and run def function(s)
-    *   Set up aws S3 to download/sync by accessing to scripts buckets (aws cli || boto3)
-    *   Scripts should only run the specific request (for yuhan: need to gather specifications)
-    *   Importing a custom python script which is not installed in a directory
-  
+## To be completed by 24 Oct 2018 (Wednesday) 
+
+- [ ] Josie's 
+    * request to be formatted in json and access to data_s2
+    * Define Product_name (Filter out date with regex), utm_zone, lat_band, grid_sq as variables 
+    * Pass the variables to downloadS2 function and run it using asyncio
+    * Parse output image and run sen2cor to process files
+- [ ]  Hafiz's
+    * Check that it is running on data.json  
+
+- [ ]   Include Error Handler** to stop script, and send notifications to josie and hafiz
+    
 ## On-hold 
-- [ ]   Testing: Script with Mock request, to check that it is downloads the script and run the functions, check if the output is correct and store in aws bucket   
-- [ ]   Create a checklist that updates on job status on Tasking Page 
-- [ ]   Include Error Handler** to stop script, and send notification to josie and hafiz
-- [ ]   Create a function to email with SMTPlib to client once successful stored in database 
+- [ ]   Intergration Testing: Run ONE and MORE Mock request from front-end, to check that it downloads the function from s3 bucket script and runs the correct functions, check if the output is correct and store in aws bucket  
+- [ ]   Parse as response to tasking_server completejob to email client 
 - [ ]   Output as Final Images should be stored in S3 bucket 
 - [ ]   Output as Final Images to be 'POST' response to mapping_server to display 
+- [ ]   Create a Checklist front page that updates on job status on Tasking Page 
--- main.py
+++ main.py
@@ -1,32 +1,84 @@
-from bottle import run, get, post, request 
+from bottle import run, get, post, request  
+from datetime import datetime
+import importlib 
 import json
+import subprocess
+import re 
+import os
 
-data = json.load(open('json_tasking_input.json', 'r'))
+req = {
+    "api_key": "",
+    "job_id": 6,
+    "organization_id": 1,
+    "organization_key": "demo",
+    "region_key": "Johor",
+    "mode": "s2",
+    "analysis_key": ["flood_risk", "plant_productivity", "foilar_moisture"],
+    "data_s2": [{
+        "product_name": "S2A_MSIL1C_20180903T031541_N0206_R118_T48NUH_20180903T061615",
+        "utm_zone": 48,
+        "latitude_band": "N",
+        "grid_square": "UH"
+    }, {
+        "product_name": "S2A_MSIL1C_20180903T031541_N0206_R118_T48NUH_20180903T061615",
+        "utm_zone": 48,
+        "latitude_band": "N",
+        "grid_square": "UH"
+    }]
+}
 
-# Get Specific Scripts from S3 Based on the tasking_server's analysis key 
-@get('/tasking')
-def getAll():
-	return {'data': data}
-
-
+# POST Request from Tasking_server Scripts from S3 Based on the tasking_server's analysis key 
 @post('/tasking')
-def inputNew(): 
-	new_request= {'data' : ''}
-	data.append(new_request)
-	return {'data' : data}
+def postReq(): 
+	req = request.json
+	print (req) 
+	return "task request received!"
 
+# Command to sync S3 Bucket, using Subprocess module popen. output should download script in awscriptpy folder
+p = subprocess.Popen(['aws', 's3', 'sync','s3://adatos-agri-services/scripts','awspyscript'], stdout=subprocess.PIPE)
+print (p.communicate())
 
-# Run and Async Josie's Script 
+# Import Dynamically to main.py to import Josie's Script from S3 Bucket 
+sample = importlib.import_module(".sample","awspyscript")
+josie_script = importlib.import_module(".SentinelHubSingleDownload","awspyscript")
+
+# Define tasking_server json as required variable 
+date = req['data_s2'][0]['product_name'] 
+
+def date_check(date):
+    pattern = '_([0-9]{8})T'
+    dtd_list = re.findall(pattern, date)
+    dtd = dtd_list[0]
+    f_dtd = datetime.strptime(dtd, '%Y%m%d').strftime('%Y-%m-%d')
+    return f_dtd
+
+tiledate = date_check(date)
+ 
+utm = str(req['data_s2'][0]['utm_zone'])
+lat = req['data_s2'][0]['latitude_band']
+grid = req['data_s2'][0]['grid_square']
+
+tilename = utm + lat + grid
+
+# create a file to store josie's script 
+downloadloc = r'./awsfolder'
+if not os.path.exists(downloadloc):
+    os.makedirs(downloadloc)
+
+# Parse in variable as argument in josie function and Run Async Josie's Script 
+
+# Run Output in sen2Cor 
 
 # Run Hafiz's Script 
 
-# Error Handler to end script and notify josie & hafiz 
+# *NEW REQUEST PENDING* 
 
-# Post to AWS Script 
+# Error Handler to end script and notify josie & hafiz error** 
 
+# USING REQUESTS Lib: 
+# Seed Folder 
+# Post to AWS S3 Bucket  
 # Post to mapping_server
-
-# python SMTP to client 
-
+# python post to completejobrequest, parse in api keys and jobid to trigger email 
 
 run(host='localhost', port=8000, reloader=True, debug=True)
